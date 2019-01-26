2ffdd0aed0ef71f91882044b05d46ce3606ebcbe
Dan Kats <djk175@dans-mbp.attlocal.net>
2019-01-25 20:07:03 -0500

    Main logo

---
+++ js/customscroll.js
@@ -0,0 +1,3 @@
+function customscroll(id) {
+	document.getElementById(id).scrollIntoView();
+}
---
+++ js/header.js
@@ -0,0 +1,18 @@
+function header(level = 0) {
+	if(level == 0) {
+		dir = '';
+	} else if(level == -1) {
+		dir = '../';
+	}
+
+	document.write('<header>');
+	document.write('<ul class="menu">');
+	document.write('	<li class="noclick">Dan Kats</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'files/CV.pdf\')" id="header_cv">CV</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Projects</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'research.html\',\'_self\')" id="header_res">Research</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'hobbies.html\',\'_self\')" id="header_extracurr">Hobbies</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'thoughts.html\',\'_self\')" id="header_thoughts">My Thoughts</li>');
+	document.write('</ul>');
+	document.write('</header>');
+}
