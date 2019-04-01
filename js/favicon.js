d41c97b2e647079c3921087a3106c16b4580ff5a
Dan Kats <djk175@Dans-MBP.home>
2019-03-31 22:03:17 -0400

    Added favicon and changed tabs

---
+++ js/favicon.js
@@ -0,0 +1,7 @@
+document.write('<link rel="apple-touch-icon" sizes="180x180" href="files/favicon/apple-touch-icon.png">');
+document.write('<link rel="icon" type="image/png" sizes="32x32" href="files/favicon/favicon-32x32.png">');
+document.write('<link rel="icon" type="image/png" sizes="16x16" href="files/favicon/favicon-16x16.png">');
+document.write('<link rel="manifest" href="files/favicon/site.webmanifest">');
+document.write('<link rel="mask-icon" href="files/favicon/safari-pinned-tab.svg" color="#5bbad5">');
+document.write('<meta name="msapplication-TileColor" content="#2b5797">');
+document.write('<meta name="theme-color" content="#ffffff">');
--- js/header.js
+++ js/header.js
@@ -8,11 +8,12 @@ function header(level = 0) {
 	document.write('<header>');
 	document.write('<ul class="menu">');
 	document.write('	<li class="noclick">Dan Kats</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'who.html\',\'_self\')" id="header_proj">Who is Dan?</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'files/CV.pdf\')" id="header_cv">CV</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Projects</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'research.html\',\'_self\')" id="header_res">Research</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'hobbies.html\',\'_self\')" id="header_extracurr">Hobbies</li>');
-	document.write('	<li onclick="window.open(\'' + dir + 'thoughts.html\',\'_self\')" id="header_thoughts">My Thoughts</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'thoughts.html\',\'_self\')" id="header_thoughts">Dan\'s Thoughts</li>');
 	document.write('</ul>');
 	document.write('</header>');
 }
