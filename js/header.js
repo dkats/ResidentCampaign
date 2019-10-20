9713cb3075645d83d0be01ff49d936ad93e72dfc
Dan Kats <djk175@Dans-MacBook-Pro.local>
2019-10-19 23:15:11 -0400

    Added M4 plans

--- js/header.js
+++ js/header.js
@@ -10,6 +10,7 @@ function header(level = 0) {
 	document.write('	<li class="noclick">Dan Kats</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'who.html\',\'_self\')" id="header_who">Who is Dan?</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'files/CV.pdf\')" id="header_cv">CV</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'fourth-year.html\',\'_self\')" id="header_fourth">Fourth Year Plans</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Projects</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'research.html\',\'_self\')" id="header_res">Research</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'hobbies.html\',\'_self\')" id="header_extracurr">Hobbies</li>');
