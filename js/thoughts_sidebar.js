9ecbd2248383b689914fc41897164f0e6dd811af
Dan Kats <djk175@Dans-MacBook-Pro.local>
2019-10-21 18:27:35 -0400

    My First Death

--- js/thoughts_sidebar.js
+++ js/thoughts_sidebar.js
@@ -11,10 +11,11 @@ function thoughts_sidebar(level = 0) {
 	document.write('		<li onclick="window.open(\'' + dir + 'Dream-Hospital.html\',\'_self\');">My Dream Hospital</li>');
 	document.write('		<li class="category">Patient Stories</li>');
 	document.write('			<ul>');
-	document.write('			<li onclick="window.open(\'' + dir + 'Insuring-Poverty.html\',\'_self\');">Insuring Poverty</li>');
-	document.write('			<li onclick="window.open(\'' + dir + 'Electronic-Health-Wreck.html\',\'_self\');">Electronic Health Wreck</li>');
-	document.write('			<li onclick="window.open(\'' + dir + 'The-Executioner.html\',\'_self\');">The Executioner</li>');
+	document.write('			<li onclick="window.open(\'' + dir + 'My-First-Death.html\',\'_self\');">My First Death</li>');
 	document.write('			<li onclick="window.open(\'' + dir + 'Irreversible-Psychosis.html\',\'_self\');">Irreversible Psychosis</li>');
+	document.write('			<li onclick="window.open(\'' + dir + 'The-Executioner.html\',\'_self\');">The Executioner</li>');
+	document.write('			<li onclick="window.open(\'' + dir + 'Electronic-Health-Wreck.html\',\'_self\');">Electronic Health Wreck</li>');
+	document.write('			<li onclick="window.open(\'' + dir + 'Insuring-Poverty.html\',\'_self\');">Insuring Poverty</li>');
 	document.write('			</ul>');
 	// document.write('		<li class="category">General Thoughts</li>');
 	// document.write('			<ul>');
